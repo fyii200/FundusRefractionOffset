9c38e543a0c6c612884301b0453d6fa57f3c3c36
Fabian Yii 俞思良 <72454128+fyii200@users.noreply.github.com>
2024-12-26 11:59:37 +0000

    Update main.sh

--- FERmodel/main.sh
+++ FERmodel/main.sh
@@ -8,14 +8,14 @@ date
 echo "############ Train (cross-validation) #############"
 python crossVal.py --plotLosses --imageDir /opt/notebooks/images/ --numWorkers 16 --kFolds 5 --numEpochs 50 
 
-echo "############# Inference: Normal test set ##############"
+echo "########## Internal inference: train/val set ##########"
+python inference.py --imageDir /opt/notebooks/images/ --numWorkers 16 --dataFile /opt/notebooks/retinalRefractionGap/data/retinalRefractionGap_train_new.csv --testWhatDataset train
+
+echo "############# Internal inference: UK Biobank unseen set ##############"
 python inference.py --TTA --imageDir /opt/notebooks/images/ --numWorkers 16 --dataFile /opt/notebooks/retinalRefractionGap/data/retinalRefractionGap_test_new.csv --testWhatDataset test 
 
-echo "############# Inference: PM test set ##############"
-python inference.py --TTA --imageDir /opt/notebooks/images/ --numWorkers 16 --dataFile /opt/notebooks/retinalRefractionGap/data/retinalRefractionGap_PMtest.csv --testWhatDataset testPM 
-
-echo "########## Inference: train/val set ##########"
-python inference.py --imageDir /opt/notebooks/images/ --numWorkers 16 --dataFile /opt/notebooks/retinalRefractionGap/data/retinalRefractionGap_train_new.csv --testWhatDataset train
+echo "############# External inference: Caledonian (external) dataset ##############"
+python inferenceExternal.py --TTA 
 
 
 
